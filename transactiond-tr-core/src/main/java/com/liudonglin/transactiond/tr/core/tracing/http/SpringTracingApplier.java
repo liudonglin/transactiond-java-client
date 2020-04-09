e489d6f0090b88e0614f1fea42feb33099696384
liudonglin <xiayangqiushi@hotmail.com>
2020-04-09 21:22:56 +0800

    update 拦截获取groupid

--- transactiond-tr-core/src/main/java/com/liudonglin/transactiond/tr/core/tracing/http/SpringTracingApplier.java
+++ transactiond-tr-core/src/main/java/com/liudonglin/transactiond/tr/core/tracing/http/SpringTracingApplier.java
@@ -7,11 +7,14 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ConditionalOnClass(HandlerInterceptor.class)
 @Component
 public class SpringTracingApplier implements HandlerInterceptor, WebMvcConfigurer {
 
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         Tracings.apply(request::getHeader);
         return true;
--- transactiond-tr-samples/transactiond-tr-samples-account/src/main/java/com/liudonglin/transactiond/tr/samples/account/AccountStartup.java
+++ transactiond-tr-samples/transactiond-tr-samples-account/src/main/java/com/liudonglin/transactiond/tr/samples/account/AccountStartup.java
@@ -1,11 +1,13 @@
 package com.liudonglin.transactiond.tr.samples.account;
 
+import com.liudonglin.transactiond.tr.core.config.EnableDistributedTransaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.liudonglin.transactiond.tr.samples.account.dao")
 @SpringBootApplication
+@EnableDistributedTransaction
 public class AccountStartup {
     public static void main(String[] args) {
         SpringApplication.run(AccountStartup.class, args);
