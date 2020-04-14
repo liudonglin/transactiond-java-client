68c94d3437b53f5f012e1f0e5cd2a19b3ab8545a
liudonglin <xiayangqiushi@hotmail.com>
2020-04-14 15:47:15 +0800

    update 实现服务端数据推送

--- transactiond-tr-core/src/main/java/com/liudonglin/transactiond/tr/core/txmsg/GRpcMessenger.java
+++ transactiond-tr-core/src/main/java/com/liudonglin/transactiond/tr/core/txmsg/GRpcMessenger.java
@@ -1,16 +1,16 @@
 package com.liudonglin.transactiond.tr.core.txmsg;
 
 import com.liudonglin.transactiond.tr.core.config.DTXClientConfig;
+import com.liudonglin.transactiond.tr.core.support.BeanHelper;
+import com.liudonglin.transactiond.tr.core.transaction.TransactionCleanService;
 import com.liudonglin.transactiond.tr.core.transaction.TransactionModel;
 import com.liudonglin.transactiond.tr.core.transaction.TransactionState;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import txmsg.CreateGroupMessage;
-import txmsg.JoinGroupMessage;
-import txmsg.ManageServiceGrpc;
-import txmsg.NotifyGroupMessage;
+import txmsg.*;
 
 @Component
 @Slf4j
@@ -20,8 +20,11 @@ public class GRpcMessenger implements ReliableMessenger  {
 
     private ManageServiceGrpc.ManageServiceBlockingStub manageServiceClient;
 
-    public GRpcMessenger(DTXClientConfig clientConfig) {
+    private BeanHelper beanHelper;
+
+    public GRpcMessenger(DTXClientConfig clientConfig, BeanHelper beanHelper) {
         this.clientConfig = clientConfig;
+        this.beanHelper = beanHelper;
 
         String managerAddress = clientConfig.getManagerAddress().get(0);
         String host = managerAddress.split(":")[0];
@@ -31,7 +34,11 @@ public class GRpcMessenger implements ReliableMessenger  {
                 .forAddress(host, port)
                 .usePlaintext()
                 .build();
+
+        //managedChannel.isTerminated()
+
         manageServiceClient = ManageServiceGrpc.newBlockingStub(managedChannel);
+        this.buildNotifyUnitConnection(managedChannel);
     }
 
     @Override
@@ -62,5 +69,35 @@ public class GRpcMessenger implements ReliableMessenger  {
                 .setState(txmsg.TransactionState.valueOf(state.value()))
                 .build();
         manageServiceClient.notifyGroup(msg);
+    }
+
+    private void buildNotifyUnitConnection(ManagedChannel managedChannel) {
+        ManageServiceGrpc.ManageServiceStub client = ManageServiceGrpc.newStub(managedChannel);
+
+        NotifyUnitMessage msg = NotifyUnitMessage
+                .newBuilder()
+                .build();
+
+        StreamObserver<NotifyUnitMessage> responseObserver = new StreamObserver<NotifyUnitMessage>() {
+            @Override
+            public void onNext(NotifyUnitMessage responseMessage) {
+                log.debug("get notify unit");
+
+                TransactionCleanService clean = beanHelper.loadTransactionCleanService(TransactionModel.LCN);
+                clean.clear(responseMessage.getGroupId(),responseMessage.getState().getNumber());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.debug("notify unit completed");
+            }
+        };
+
+        client.notifyUnit(msg,responseObserver);
     }
 }
